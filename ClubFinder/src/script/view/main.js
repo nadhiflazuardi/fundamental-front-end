c8509e5e140b48c2224210f7ccbff9392cb7d70b
Nadhif Lazuardi <berkasnadhif@gmail.com>
2023-12-20 06:36:33 +0700

    object destructuring on club finder app

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -11,18 +11,10 @@ const main = function () {
   const renderResult = function (results) {
     clubListElement.innerHTML = '';
     results.forEach(function (club) {
-      const name = club.name;
-      const fanArt = club.fanArt;
-      const description = club.description;
+      const {name, fanArt, description} = club;
 
       const clubElement = document.createElement('div');
       clubElement.setAttribute('class', 'club');
-
-      // clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-      //     '<div class="club-info">\n' +
-      //     '<h2>' + name + '</h2>\n' +
-      //     '<p>' + description + '</p>' +
-      //     '</div>';
 
       clubElement.innerHTML = `
           <img class="fan-art-club" src=" ${fanArt} " alt="Fan Art">
